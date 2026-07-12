848cd257938b0f42f9d6adb4c877f2a3bcfac830
Chinmaya Behera <37406952+Chinmaya56789@users.noreply.github.com>
2026-07-12 16:35:00 +0530

    Time: 25 ms (31.80%) | Memory: 47.2 MB (17.01%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,47 +1,25 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int i =0 ;
-         int j =0 ;
-         char maxc=0;
-         int maxcount =0;
-         int ans = Integer.MIN_VALUE;
-         Map<Character,Integer> map = new HashMap<>();
-         int total =0;
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int maxCount = 0;
+        int maxLen = 0;
 
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            maxCount = Math.max(maxCount, map.get(c));
 
-         while (i < s.length()){
-            char c = s.charAt(i);
-            int  temp = map.getOrDefault(c ,0);
-             map.put(c ,++temp);
-            if (temp > maxcount){
-                maxc = c ;
-                maxcount =temp;
+            // Shrink window if characters to replace > k
+            while (right - left + 1 - maxCount > k) {
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                left++;
             }
-            i++;
 
-            total++;
-            while(j <s.length() && k < (total-maxcount)){
-                c = s.charAt(j);
-                temp = map.get(c);
-                map.put (c ,--temp);
-                if (c == maxc){
-                    for(char ct :map.keySet()){
-                        if (maxcount < map.get(ct)){
-                            maxc = ct ;
-                            maxcount = map.get(ct);
-                        }
-                    }
-                }
-                total--;
-                j++;
-                
-            }
-        ans = Math.max(ans , i -j);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
 
-
-
-         }
-
-         return ans;
+        return maxLen;
     }
 }
