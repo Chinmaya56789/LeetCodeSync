3ea3da85e903659c218224c6a27cab5cdda2d1f6
Chinmaya Behera <37406952+Chinmaya56789@users.noreply.github.com>
2026-07-15 01:00:32 +0530

    Time: 332 ms (5.00%) | Memory: 48.4 MB (5.09%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,64 +1,55 @@
 class Solution {
-    public String minWindow(String s, String t) {
-        Map<Character , Integer> map  = new HashMap<>();
-        Map<Character , Integer> map2 = new HashMap<>();
-        int l =0 , r =0;
+    public String minWindow(String sti, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        int l = 0, r = 0;
         int length = Integer.MAX_VALUE; 
-        String  ans = ""; 
-        for (char c  : t.toCharArray()){
-            map.put(c , map.getOrDefault(c ,0)+1);
+
+        int st = 0;
+        int ed = 0; 
+
+        char[] s = sti.toCharArray();
+        for (char ch : t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
-        
-        while (r < s.length()){
+        while (r < s.length) {
+            char cl = s[r];
 
-            char c = s.charAt(r);
+            if (map.containsKey(cl)) {
+                map2.put(cl, map2.getOrDefault(cl, 0) + 1);
+            }
 
-            if (map.containsKey(c))
-            map2.put(c , map2.getOrDefault(c ,0)+1);
-
-            //  System.out.println(" OUT map2 :" +map2 +" l :" +l +" r:"+ r + " ans : " +ans +" cpmpare :" +mapCompare(map , map2 ) ) ;
-            
-            while(mapCompare(map , map2 ) && l <= r  ){
-
-
-               
-                if (map.containsKey(s.charAt(l))  ){
-
-                    map2.put(s.charAt(l) ,map2.get(s.charAt(l))-1);
-
-                    if (length> r-l +1 ) {
-                        ans =s.substring (l ,r+1 );
-                    length = ans.length();
-                    }
-                    
-
-                    
-
+            while (mapCompare(map, map2) && l <= r) {
+                // 1. Update the best window BEFORE modifying frequency/pointers
+                if (length > r - l + 1) {
+                    st = l;
+                    ed = r;
+                    length = r - l + 1;
                 }
-                     
 
-                     l++;
+                // 2. Decrement frequency if the left character is part of target 't'
+                if (map.containsKey(s[l])) {
+                    map2.put(s[l], map2.get(s[l]) - 1);
+                }
 
-                    //   System.out.println("In map2 : " +map2 +" l :" +l +" r:"+ r + " ans : " +ans ) ;
+                l++;
             }
 
             r++;
         }
-        return ans ;
 
+        // 3. Return empty string if no valid window was found
+        return length == Integer.MAX_VALUE ? "" : new String(Arrays.copyOfRange(s, st, ed + 1));
     }
 
-
-    public static boolean mapCompare(Map<Character , Integer > map1 ,Map<Character , Integer > map2){
-        for (Map.Entry<Character , Integer> entry : map1.entrySet()){
+    public static boolean mapCompare(Map<Character, Integer> map1, Map<Character, Integer> map2) {
+        for (Map.Entry<Character, Integer> entry : map1.entrySet()) {
             int val = entry.getValue();
-
-            if (!map2.containsKey(entry.getKey()) || map2.get(entry.getKey()) < val)
-            return false ;
-
+            if (!map2.containsKey(entry.getKey()) || map2.get(entry.getKey()) < val) {
+                return false;
+            }
         }
-
-        return true ;
+        return true;
     }
 }
