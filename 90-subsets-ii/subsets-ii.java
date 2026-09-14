d56fed8cfaa3b01033c2fe992552efaed72fbe08
Chinmaya Behera <37406952+Chinmaya56789@users.noreply.github.com>
2026-09-14 23:43:06 +0530

    Time: 2 ms (99.73%) | Memory: 45 MB (76.48%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -11,10 +11,8 @@ class Solution {
             List<List<Integer>> temp = new ArrayList<List<Integer>> (ans);
             int startIndex =0 ;
             
-            if( i >0 && nums[i] ==nums[i-1]){
+            if( i >0 && nums[i] ==nums[i-1])
                 startIndex = temp.size() - lastIterations;
-                System.out.println(startIndex);
-            }
             lastIterations =0;    
             for (int j = startIndex ; j< temp.size() ;j++){
                 List<Integer> subTemp = new ArrayList<>(temp.get(j));
